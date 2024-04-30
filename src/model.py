dcfe43996aa52709c184e0325dc6bf32834d832b
h-albert-lee <gksdnf424@gmail.com>
2024-04-30 23:02:59 +0900

    - minor bug fix - reconstructed experiment module

--- src/model.py
+++ src/model.py
@@ -4,14 +4,17 @@ import torch
 
 class ModelModule:
     def __init__(self, model_ckpt, gpu_args):
+        self.tensor_parallel_size = self._check_parallel_devices(gpu_args)
         self.model_ckpt = model_ckpt
         self.model = self.load_model()
-        self.tensor_parallel_size = self._check_parallel_devices(gpu_args)
 
     def load_model(self):
         """Load LLM """
         llm = LLM(self.model_ckpt, tensor_parallel_size=self.tensor_parallel_size)
-        return models.VLLM(llm)
+        return llm
+    
+    def load_outlines_model(self):
+        return models.VLLM(self.model)
 
     def generate_answers(self, questions, sampling_params):
         outputs = self.model.generate(questions, sampling_params)
