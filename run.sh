ac7f94a9b09ba3465c6f250932c15ffc636af73a
sklim84 <captiong84@gmail.com>
2024-05-04 10:00:08 +0900

    modify report path

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 prompts_file='./config/prompt_addt.yml'
 
-# 1 2 19 24 33 34 41 42 58 59
+## 1 2 19 24 33 34 41 42 58 59
 for config_idx in 1 2 19 24 33 34 41 42 58 59; do
   config_file='./config/experiment_configs/config_'$config_idx'.yml'
   exp_report_file='./exp/report_config_'$config_idx'_addt_prompts.csv'
