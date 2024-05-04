53c92e18db57274c39808da6ff0f74a95372d9b4
sklim84 <captiong84@gmail.com>
2024-05-04 09:59:25 +0900

    add report example and fix bug

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ prompts_file='./config/prompt_addt.yml'
 # 1 2 19 24 33 34 41 42 58 59
 for config_idx in 1 2 19 24 33 34 41 42 58 59; do
   config_file='./config/experiment_configs/config_'$config_idx'.yml'
-  exp_report_file='/exp/report_config_'$config_idx'_addt_prompts.csv'
+  exp_report_file='./exp/report_config_'$config_idx'_addt_prompts.csv'
 
   python -u main.py \
     --config_file $config_file \
