e4baf28bcb7865d2d401ba1737dfff6f20963386
Suzie Oh <59340911+ohsuz@users.noreply.github.com>
2024-05-12 21:38:47 +0900

    Update args.py

--- config/args.py
+++ config/args.py
@@ -6,7 +6,7 @@ parser.add_argument("--config_file", type=str, default="./config/config.yml", he
 parser.add_argument("--prompts_file", type=str, default="./config/prompt.yml", help="Path to the prompts YAML file")
 parser.add_argument("--exp_report_file", type=str, default="./exp/report.csv", help="Path to the experimental results file")
 parser.add_argument("--num_gpus", type=str, default="auto", help = "Number of GPUs for distributed inference")
-parser.add_argument("--exp", type=str, default="mc", help = "Experiment Options (current options: cot, mc, sc-3, sc-5, ...)")
+parser.add_argument("--exp_settings", type=str, default="mc", help = "Experiment Options (current options: cot, mc, sc-3, sc-5, ...)")
 parser.add_argument("--use_vllm", type=bool, default=True, help= "Check if you want to use vLLM for inference")
 parser.add_argument("--model_cache_dir", type=str, default=None, help= "Assign huggingface cache directory, if needed")
 parser.add_argument("--model_branch", type = str, default=None, help= "Used when you want to utilize models other than the main branch in huggingface")
